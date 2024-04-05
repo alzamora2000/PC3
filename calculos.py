935c6326d0f8355979759b1e4e34a5f0bce79f87
alzamora2000 <162034762+alzamora2000@users.noreply.github.com>
2024-04-05 21:54:36 +0000

    calculos

---
+++ calculos.py
@@ -0,0 +1,10 @@
+import solucion9
+
+num1 = int(input("Ingrese el primer número a operar: "))
+num2 = int(input("Ingrese el segundo número a operar: "))
+
+print("Suma:", solucion9.suma(num1, num2))
+print("Resta:", solucion9.resta(num1, num2))
+print("Producto:", solucion9.producto(num1, num2))
+print("División:", solucion9.division(num1, num2))
+
